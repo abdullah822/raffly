79d961889bb8c43aba65bb7f6d444f7d05393a1b
vilimika <viliusmika@gmail.com>
2020-10-28 15:32:30 -0700

    added size check and push into db when raffle is entered

--- src/enterRaffle.php
+++ src/enterRaffle.php
@@ -2,9 +2,10 @@
 include "mongod.inc";
 $raffleID = $_POST['raffle_id'];
 $user = $_SESSION['username'] ;
+$size = $_POST['size'] ?? false;
 
 $db = new Mongod;
 $client = $db->connect();
-$db->enterRaffle($raffleID, $user, $client);
+$db->enterRaffle($raffleID, $user, $client, $size);
 
 ?>
