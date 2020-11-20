5041e07f9f7f2705c22379e768d1c7133b5effe6
vilimika <viliusmika@gmail.com>
2020-11-19 22:06:13 -0800

    added g-recaptcha check

--- src/register.php
+++ src/register.php
@@ -1,7 +1,25 @@
 <?php
 include "mongod.inc";
+define("RECAPTCHA_V3_SECRET_KEY", '6Ld3jNkZAAAAAH2ThcRFyDXkiNCyr1GjJrYqeJOY');
 
+$token = $_POST['g-recaptcha-response'];
 
-$db = new Mongod;
-$client = $db->connect();
-$db->insertNewUser($_POST, $client);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,"https://www.google.com/recaptcha/api/siteverify");
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array('secret' => RECAPTCHA_V3_SECRET_KEY, 'response' => $token)));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($ch);
+curl_close($ch);
+$arrResponse = json_decode($response, true);
+
+if($arrResponse["success"] == '1' && $arrResponse["score"] >= 0.5)
+{
+  $db = new Mongod;
+  $client = $db->connect();
+  $db->insertNewUser($_POST, $client);
+}
+else
+{
+  echo "spam";
+}
