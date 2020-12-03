ed63a2174972066bcdf632fecd31c21274ddc69c
tim707 <60167722+tim707@users.noreply.github.com>
2020-12-03 01:51:57 -0500

    Update and rename winner.html to winner.php
    Added in automatic product selection to facilitate token emails

---
+++ front-end/winner.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <!-- Site made with Mobirise Website Builder v5.2.0, https://mobirise.com -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v5.2.0, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="assets/images/favicon-128x97.png" type="image/x-icon">
+  <meta name="description" content="Raffles">
+  
+  
+  <title>Raffly Raffles | Winner</title>
+  <link rel="stylesheet" href="assets/web/assets/mobirise-icons2/mobirise2.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/web/assets/gdpr-plugin/gdpr-styles.css">
+  <link rel="stylesheet" href="assets/tether/tether.min.css">
+  <link rel="stylesheet" href="assets/dropdown/css/style.css">
+    <link rel="stylesheet" href="assets/socicon/css/styles.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="preload" as="style" href="assets/mobirise/css/mbr-additional.css"><link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+  
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-180949359-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-180949359-1');
+</script>
+
+  
+</head>
+<body onload="showProductsOnLoad()">
+
+<!-- Analytics -->
+<script src="assets/js/getuser.js"></script>
+<!-- /Analytics -->
+
+
+  
+  <section class="menu cid-sgBT8rptcu" once="menu" id="menu2-1c">
+
+    
+
+    <nav class="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top navbar-toggleable-sm bg-color transparent">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <div class="hamburger">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </button>
+        <div class="menu-logo">
+            <div class="navbar-brand">
+                
+                
+            </div>
+        </div>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent"><span id="user" style="color:white;"></span>
+            <ul class="navbar-nav nav-dropdown nav-right navbar-nav-top-padding" data-app-modern-menu="true"><li class="nav-item dropdown">
+                    <a class="nav-link link text-primary dropdown-toggle display-4" href="#" data-toggle="dropdown-submenu" aria-expanded="false"><span class="mobi-mbri mobi-mbri-user-2 mbr-iconfont mbr-iconfont-btn"></span></a><div class="dropdown-menu"><a class="text-primary dropdown-item display-4" href="index.html">Home</a><a class="text-primary dropdown-item display-4" href="index.html">Activity</a><a class="text-primary dropdown-item display-4" href="shop.html" aria-expanded="false">Raffles</a><a class="text-primary dropdown-item display-4" href="middle/php/logout.php">Logout</a><a class="text-primary dropdown-item display-4" href="feedback.html">Feedback</a></div>
+                </li></ul>
+            
+        </div>
+    </nav>
+</section>
+
+<section class="services1 cid-sgBT8s2Fbk mbr-parallax-background" id="services1-1d">
+    <!---->
+    
+    <!---->
+    <!--Overlay-->
+    <div class="mbr-overlay" style="opacity: 0.7; background-color: rgb(7, 39, 76);">
+    </div>
+    <!--Container-->
+    <div class="container">
+        <div class="row justify-content-center">
+            <!--Titles-->
+            <div class="title pb-5 col-12">
+            <h2 class="align-left pb-3 mbr-fonts-style display-2">               <div><br></div>
+                <div>You Won!</div></h2>
+                <h3 class="mbr-section-subtitle mbr-light mbr-fonts-style display-7">
+                    Please enter your winners token to complete purchase.
+                </h3>
+                <input type="text" name="token" id="token" autocomplete="off" style="margin:4px; min-width:280px;">
+                <button type="button" class="btn btn-sm btn-primary" onclick="showProduct(document.getElementById('token').value);">Enter</button>
+                <input id="tok_2" name="tok_2" form="tok" autocomplete="off" tabindex="-1" style="width:0px;
+  height: 0px;  position: absolute;  margin-top: -250px;">
+            </div>
+
+<div id="result" style="min-height:300px;">
+    <p>No raffles found</p>
+</div>
+                 
+    </div>
+</div>
+</section>
+
+<section once="footers" class="cid-sgBT8sjGyd" id="footer7-1e">
+
+    
+
+    
+
+    <div class="container">
+        <div class="media-container-row align-center mbr-white">
+            <div class="row row-links">
+                <ul class="foot-menu">
+                    
+                    
+                    
+                    
+                    
+                <li class="foot-menu-item mbr-fonts-style display-7">
+                        <a class="text-white mbr-bold" href="#" target="_blank">About Us</a>
+                    </li><li class="foot-menu-item mbr-fonts-style display-7"><a class="text-white mbr-bold" href="#" target="_blank">Mission</a></li><li class="foot-menu-item mbr-fonts-style display-7"><a class="text-white mbr-bold" href="#" target="_blank">Partners</a></li></ul>
+            </div>
+            <div class="row social-row">
+                <div class="social-list align-right pb-2">
+                    
+                    
+                    
+                    
+                    
+                    
+                <div class="soc-item">
+                        <a href="https://twitter.com" target="_blank">
+                            <span class="mbr-iconfont mbr-iconfont-social socicon-twitter socicon"></span>
+                        </a>
+                    </div><div class="soc-item">
+                        <a href="https://www.facebook.com" target="_blank">
+                            <span class="mbr-iconfont mbr-iconfont-social socicon-facebook socicon"></span>
+                        </a>
+                    </div><div class="soc-item">
+                        <a href="https://www.youtube.com" target="_blank">
+                            <span class="mbr-iconfont mbr-iconfont-social socicon-youtube socicon"></span>
+                        </a>
+                    </div><div class="soc-item">
+                        <a href="https://instagram.com" target="_blank">
+                            <span class="mbr-iconfont mbr-iconfont-social socicon-instagram socicon"></span>
+                        </a>
+                    </div></div>
+            </div>
+            <div class="row row-copirayt">
+                <p class="mbr-text mb-0 mbr-fonts-style mbr-white align-center display-7">
+                    Â© Copyright 2020 Raffly - All Rights Reserved
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<script src="assets/web/assets/jquery/jquery.min.js"></script>
+  <script src="assets/popper/popper.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/web/assets/cookies-alert-plugin/cookies-alert-core.js"></script>
+  <script src="assets/web/assets/cookies-alert-plugin/cookies-alert-script.js"></script>
+  <script src="assets/tether/tether.min.js"></script>
+  <script src="assets/smoothscroll/smooth-scroll.js"></script>
+  <script src="assets/dropdown/js/nav-dropdown.js"></script>
+  <script src="assets/dropdown/js/navbar-dropdown.js"></script>
+  <script src="assets/touchswipe/jquery.touch-swipe.min.js"></script>
+        <script src="assets/parallax/jarallax.min.js"></script>
+  <script src="assets/theme/js/script.js"></script>
+    
+  <script>
+document.getElementById("token").value = "";
+</script>
+<script src="assets/js/winner.js"></script>
+
+    
+<input name="cookieData" type="hidden" data-cookie-customDialogSelector='null' data-cookie-colorText='#424a4d' data-cookie-colorBg='rgba(234, 239, 241, 0.99)' data-cookie-textButton='Agree' data-cookie-colorButton='' data-cookie-colorLink='#424a4d' data-cookie-underlineLink='true' data-cookie-text="We use cookies to give you the best experience. Read our <a href='privacy.html'>cookie policy</a>.">
+<script>
+function showProductsOnLoad(){
+   var tok=<?php echo json_encode($_GET['t']); ?>;
+   fetch('../middle/php/getRafflesComplete.php?t='+tok, {
+    method: "GET",
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    }
+  })
+    .then((res) => { return res.json() })
+    .then((data) => {
+      count = 0;
+      let result = `<div></div>`;
+      finish = [];
+      
+      data.forEach((raffle) => {
+        const { id, title, description, startTime, endTime, company, productImage, quantity, size, format, unit_cost, productName, } = raffle;
+        end = new Date(endTime.$timestamp.t * 1000);
+        var cost = unit_cost.toString().replace('$', "");
+        raffle_id = id.$oid;
+        count++;
+        var ids=tok.slice(6);
+        if(ids == raffle_id){ 
+        result +=
+          `<div class="card col-6 p-3 " style="display:block; color:white; margin: 0 auto; ">
+              <div id="prod-${count}"></div>
+                <span><h5 style="color:white;"> ${title} </h5></span>
+                  <div class="" >
+                    <div class="card-wrapper">
+                      <div class="card-img">
+                        <img src="${productImage}" alt="" title="">
+                      </div>
+                      <div class="card-box" style="padding:4px;">
+                        <p class="mbr-text mbr-fonts-style display-7" style="padding:4px;">${description}</p>
+                        <div style="text-align:center;">
+                          <form name="${raffle_id}" id="${raffle_id}" action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                          <input type="hidden" name="cmd" value="_xclick">
+                          <input type="hidden" name="business" value="rafflyraffles@gmail.com">
+                          <input type="hidden" name="lc" value="US">
+                          <input type="hidden" name="item_name" value="${productName}">
+                          <input type="hidden" name="item_number" value="${raffle_id}">
+                          <input type="hidden" name="amount" value="${cost}">
+                          <input type="hidden" name="currency_code" value="USD">
+                          <input type="hidden" name="button_subtype" value="services">
+                          <input type="hidden" name="no_note" value="0">
+                          <input type="hidden" name="tax_rate" value="0.070">
+                          <input type="hidden" name="shipping" value="10.00">
+                          <input type="hidden" name="bn" value="PP-BuyNowBF:btn_paynowCC_LG.gif:NonHostedGuest">`;
+        if (size !== false) {
+          result += `<table style="text-align:center; width:100%;">
+            <tr><td>
+            <input type="hidden" name="on0" value="Sizes"></td></tr><tr><td>Sizes:&nbsp<select name="os0" style="border-radius:8px; padding:4px;" required="required"> <option value="">Select</option>`;
+          for (var k = 0; k < size.length; k++) { 
+          result += `<option value="${size[k]}">${size[k]}</option>`; }
+        }
+        result += `</select></td></tr>
+        </table>
+        <button class="btn btn-primary btn-small" style="padding:0.5rem 2rem;">Buy</button>&nbsp&nbsp&nbsp
+                      </form>   
+                      </div>
+                  </div>
+              </div>
+          </div>
+        </div>`;}
+        
+        document.getElementById('result').innerHTML = result;
+      });
+    })
+    .catch(err => console.log('Request Failed', err)); // Catch errors
+    document.getElementById("token").value = "";
+}
+</script>
+  </body>
+</html>
